ec0c925006fc046c3b2bc95d2d89d3e844f36201
banteg <banteeg@gmail.com>
2018-10-21 19:12:33 +0700

    add contracts

---
+++ src/js/contracts.js
@@ -0,0 +1,10 @@
+export const disperse = {
+  abi: [
+    'function disperseEther(address[] recipients, uint256[] values)',
+    'function disperseToken(address token, address[] recipients, uint256[] values)',
+    'function disperseTokenSimple(address token, address[] recipients, uint256[] values)',
+  ],
+  address: {
+    5777: '0x634ad8bf795655b3d855be5e7cc97a0cdcdb6497',
+  },
+}
