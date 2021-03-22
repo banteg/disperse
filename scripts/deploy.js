0573747d0ce288e1c42cd37bba951346160fb6d5
banteg <banteeg@gmail.com>
2021-03-22 12:36:10 +0300

    chore: rm buidler script

--- scripts/deploy.js
+++
@@ -1,9 +0,0 @@
-async function task () {
-    let token = await artifacts.require('TestToken').new()
-    console.log(`token: ${token.address}`)
-    
-    let disperse = await artifacts.require('Disperse').new()
-    console.log(`disperse: ${disperse.address}`)
-}
-
-task()
