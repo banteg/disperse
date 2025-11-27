0960783c2dfa232e03c89378a2f5105a15a10af5
banteg <4562643+banteg@users.noreply.github.com>
2025-11-27 08:02:06 +0400

    chore: satisfy lint for wagmi chains

--- wagmi-disperse/src/wagmi.ts
+++ wagmi-disperse/src/wagmi.ts
@@ -1,6 +1,6 @@
-import { http, createConfig } from "wagmi";
 import * as chains from "viem/chains";
 import type { Chain } from "viem/chains";
+import { http, createConfig } from "wagmi";
 import { coinbaseWallet, injected, metaMask, walletConnect } from "wagmi/connectors";
 import { isValidChain } from "./utils/typeGuards";
 
